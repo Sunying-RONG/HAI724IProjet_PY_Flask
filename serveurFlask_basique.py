b89445250f9923956edecde56d1f42f3f703cbe7
Sunying-RONG <sunying.rong@hotmail.com>
2021-12-07 10:46:39 +0100

    search plusieurs mot clé

--- serveurFlask_basique.py
+++ serveurFlask_basique.py
@@ -37,21 +37,65 @@ def image(fichier):
 #             references.append(resultat.group())
 #    return json.dumps(references)
 
+# @app.route('/recherche/<criteres>')
+# def recherche_critere(criteres):
+#    print("/recherche/"+criteres)
+#    listecriteres = criteres.split(" ")
+#    liste = os.listdir("BD_desserts")
+#    for critere in listecriteres :
+#       lignes = []
+#       for fichier in liste :
+#          fd = open("BD_desserts/"+fichier)
+#          for ligne in fd.readlines() :
+#             res = re.search(""+critere, ligne, re.IGNORECASE)   
+#             # re.search() function will search the regex and return the first occurrence.
+#             # returns a match object when the pattern is found and “null” if the pattern is not found
+#             if res :
+#                lignes.append([fichier, ligne])  # Le nom de la page est envoyée avant la ligne dans une sous-liste
+#    return json.dumps(lignes)
+
 @app.route('/recherche/<criteres>')
 def recherche_critere(criteres):
    print("/recherche/"+criteres)
    listecriteres = criteres.split(" ")
-   liste = os.listdir("BD_desserts")
+   print("listecriteres: ", listecriteres)
+   regexTousCriteres = ""
+   regexOuCriteres = ""
    for critere in listecriteres :
-      lignes = []
-      for fichier in liste :
+      regexTousCriteres = regexTousCriteres + critere + ".*"
+      regexOuCriteres = regexOuCriteres + critere + "|"
+      # eg "aa.*cc.*" match "aa bb cc dd"
+  
+   regexTousCriteres = regexTousCriteres[:-2]
+   regexOuCriteres = regexOuCriteres[:-1]
+   print(regexTousCriteres)
+   print(regexOuCriteres)
+   liste = os.listdir("BD_desserts")
+   fichierTrouve = []
+   # afficher une ligne de fichier qui comprend tous les critères, sinon qui comprend au moins, n'import quel critère
+   lignes = []
+   for fichier in liste :
+      fd = open("BD_desserts/"+fichier)
+      # dans chaque fichier
+      # tousTrouve = False
+      # while not tousTrouve :
+      for ligne in fd.readlines() :
+         resTous = re.search(regexTousCriteres, ligne, re.IGNORECASE)
+         print(resTous)
+         if resTous :
+            lignes.append([fichier, ligne])
+            fichierTrouve.append(fichier)
+            break
+            # tousTrouve = True
+   for fichier in liste :
+      if fichier not in fichierTrouve :
          fd = open("BD_desserts/"+fichier)
          for ligne in fd.readlines() :
-            res = re.search(""+critere, ligne, re.IGNORECASE)   
-            # re.search() function will search the regex and return the first occurrence.
-            # returns a match object when the pattern is found and “null” if the pattern is not found
-            if res :
-               lignes.append([fichier, ligne])  # Le nom de la page est envoyée avant la ligne dans une sous-liste
+            res = re.search(regexOuCriteres, ligne, re.IGNORECASE)
+            print(res)
+            if res : 
+               lignes.append([fichier, ligne])
+               break
    return json.dumps(lignes)
 
 # @app.route('/hello/<param1>')  # ex. route : localhost:5000/hello/Pierre
